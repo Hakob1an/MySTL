77d6f75683dc5b8a32299ebe9795f6634d5af3e0
Hripsime Hakobyan <118827365+Hakob1an@users.noreply.github.com>
2024-01-19 21:00:48 +0400

    Update string.cpp

--- String/string.cpp
+++ String/string.cpp
@@ -1,49 +1,54 @@
-#include "string.h"
+#include "string.hpp"
 
-
-string::string() : m_onHeap{false} 
+string::string() 
+    : _isOnHeap{false}
+    , _size{0}
 {
-    m_onstack[0] = '\0';
+    _onstack[0] = '\0';
 }
-string::string(const char* str) 
+
+string::string(const_pointer str) 
 {
-    std::size_t size = std::strlen(str);
-    if (size < m_maxSize) {
-        m_onHeap = false;
-        std::strcpy(m_onstack, str);
+    size_type size = std::strlen(str);
+    if (size < _maxSize) {
+        _isOnHeap = false;
+        std::strcpy(_onstack, str);
     }
     else {
-        m_onHeap = true;
-        m_onheap.size = size;
-        m_onheap.str = new char[size + 1];
-        std::strcpy(m_onheap.str, str);
+        _isOnHeap = true;
+        _isOnHeap = new char[size + 1];
+        std::strcpy(_onheap, str);
     }
+    _size = size;
 }
+
 string ::string(const std::string& str) : string(str.c_str())
 {}
+
 string::string(const string& src)
 {
     copyFrom(src);
 }
+
 string::string(string&& src) noexcept 
 {
-    m_onHeap = src.m_onHeap;
-    if (m_onHeap) {
-        m_onheap.size = src.m_onheap.size;
-        m_onheap.str = src.m_onheap.str;
+    _isOnHeap = src._isOnHeap;
+    if (_isOnHeap) {
+        _onheap = src._onheap;
     }
     else { 
-        std::strcpy(m_onstack, src.m_onstack);
+        std::strcpy(_onstack, src._onstack);
     }
-    src.m_onHeap = false;
-    src.m_onstack[0] = '\0';
+    _size = src._size;
+    src._isOnHeap = false;
+    src._onstack[0] = '\0';
 }
+
 string::~string()
 {
-    if (m_onHeap) {
-        delete[] m_onheap.str;
-    }
+    clear();
 }
+
 string& string::operator=(const string& rhs)
 {
     if (this != & rhs) {
@@ -52,55 +57,60 @@ string& string::operator=(const string& rhs)
     
     return *this;
 }
+
 string& string::operator=(string&& rhs) noexcept 
 {
     if (this != &rhs) {
-        m_onHeap = rhs.m_onHeap;
-        if (m_onHeap) {
-            m_onheap.size = rhs.m_onheap.size;
-            m_onheap.str = rhs.m_onheap.str;
+        _isOnHeap = rhs._isOnHeap;
+        if (_isOnHeap) {
+            delete[] _onheap;
+            _onheap = rhs._onheap;
         }
         else {
-            std::strcpy(m_onstack, rhs.m_onstack);
+            std::strcpy(_onstack, rhs._onstack);
         }
-        rhs.m_onHeap = false;
-        rhs.m_onstack[0] = '\0';
+        _size = rhs._size;
+        rhs._isOnHeap = false;
+        rhs._onstack[0] = '\0';
     }
-    
     return *this;
 }
-string& string::operator=(const char* str)
+
+string& string::operator=(const_pointer str)
 {
-    std::size_t len = std::strlen(str);
+    size_type len = std::strlen(str);
 	
-	if (m_onHeap) {
-		delete[] m_onheap.str;
-		m_onHeap = false;
+	if (_isOnHeap) {
+		delete[] _onheap;
+		_isOnHeap = false;
 	}
-	if (len > m_maxSize) {
-    	m_onHeap = true;
-    	m_onheap.size = len;
-    	m_onheap.str = new char[m_onheap.size + 1];
-        std::strcpy(m_onheap.str, str);
+	if (len > _maxSize) {
+    	_isOnHeap = true;
+    	_onheap = new char[_size + 1];
+        std::strcpy(_onheap, str);
     }
     else {
-   		std::strcpy(m_onstack, str);
+   		std::strcpy(_onstack, str);
    	}
+    _size = len;
 	
     return *this;
 }
+
 string& string::operator+=(const string& s)
 {
     this->append(s.c_str());
     
     return *this;
 }
-string& string::operator+=(const char* s)
+
+string& string::operator+=(const_pointer s)
 {
     this->append(s);
     
     return *this;
 }
+
 string string::operator+(const string& s) const
 {
     string tmp{*this};
@@ -108,122 +118,157 @@ string string::operator+(const string& s) const
     
     return tmp;
 }
-string operator+(const string& s1, const char* s2)
+
+string operator+(const string& s1, string::const_pointer s2)
 {
     string tmp{s1};
     tmp += s2;
     
     return tmp;
 }
-string operator+(const char* s1, const string& s2)
+
+string operator+(string::const_pointer s1, const string& s2)
 {
     string tmp{s1};
     tmp += s2;
     
     return tmp;
 }
+
+string::const_reference string::operator[](size_type pos) const
+{
+    return _isOnHeap ? _onheap[pos] : _onstack[pos];
+}
+
+string::reference string::operator[](size_type pos)
+{
+    return const_cast<reference>(std::as_const(*this)[pos]);
+}
+
 bool string::operator==(const string& other) const
 {
     return (size() == other.size()) && (std::strcmp(c_str(), other.c_str()) == 0);
 }
-bool string::operator==(const char* str) const
+
+bool string::operator==(const_pointer str) const
 {
 	return (size() == std::strlen(str)) && (std::strcmp(c_str(), str) == 0);
 }
-bool operator==(const char* str, const string& other) 
+
+bool operator==(string::const_pointer str, const string& other) 
 {
 	return (std::strlen(str) == other.size()) && (std::strcmp(str, other.c_str()) == 0);
 }
+
 bool string::operator!=(const string& other) const
 {
 	return !(*this == other);
 }
-bool string::operator!=(const char* str) const
+
+bool string::operator!=(const_pointer str) const
 {
 	return !(*this == str);
 }
-bool operator!=(const char* str, const string& other) 
+
+bool operator!=(string::const_pointer str, const string& other) 
 {
 	return !(str == other);
 }
+
 bool string::operator<(const string& other) const
 {
 	return (size() == other.size()) && (std::strcmp(c_str(), other.c_str()) < 0);
 }
-bool string::operator<(const char* str) const
+
+bool string::operator<(const_pointer str) const
 {
 	return (size() == std::strlen(str)) && (std::strcmp(c_str(), str) < 0);
 }
-bool operator<(const char* str, const string& other) 
+
+bool operator<(string::const_pointer str, const string& other) 
 {
 	return (std::strlen(str) == other.size()) && (std::strcmp(str, other.c_str()) < 0);
 }
+
 bool string::operator<=(const string& other) const
 {
 	return (*this == other) && (*this < other);
 }
-bool string::operator<=(const char* str) const
+
+bool string::operator<=(const_pointer str) const
 {
 	return (*this == str) && (*this < str);
 }
-bool operator<=(const char* str, const string& other)
+
+bool operator<=(string::const_pointer str, const string& other)
 {
 	return (str == other) && (str < other);
 }
+
 bool string::operator>(const string& other) const
 {
 	return !(*this <= other);
 }
-bool string::operator>(const char* str) const
+
+bool string::operator>(const_pointer str) const
 {
 	return !(*this <= str);
 }
-bool operator>(const char* str, const string& other) 
+
+bool operator>(string::const_pointer str, const string& other) 
 {
 	return !(str <= other);
 }
+
 bool string::operator>=(const string& other) const
 {
 	return !(*this < other);
 }
-bool string::operator>=(const char* str) const
+
+bool string::operator>=(const_pointer str) const
 {
 	return !(*this < str);
 }
-bool operator>=(const char* str, const string& other) 
+
+bool operator>=(string::const_pointer str, const string& other) 
 {
 	return !(str < other);
 }
+
 std::ostream& operator<<(std::ostream& out, const string& str)
 {
     out << str.c_str();
     
     return out;
 }
+
 std::istream& operator>>(std::istream& in, string& str) 
 {
-    constexpr std::size_t bufferSize = 1024;
+    constexpr string::size_type bufferSize = 1024;
     
-    char buffer[bufferSize];
+    string::value_type buffer[bufferSize];
     in.getline(buffer, bufferSize);
     str = string(buffer);
 
     return in;
 }
+
 string to_string(int num) 
 {
-	constexpr std::size_t bufferSize = 32;
-	char buffer[bufferSize];
+	constexpr string::size_type bufferSize = 32;
+    string::value_type buffer[bufferSize];
 	std::snprintf(buffer, bufferSize, "%d", num);
 	return string(buffer);
 }
+
 string to_string(double num) 
 {
-	constexpr std::size_t bufferSize = 32;
-	char buffer[bufferSize];
+	constexpr string::size_type bufferSize = 32;
+    string::value_type buffer[bufferSize];
 	std::snprintf(buffer, bufferSize, "%f", num);
 	return string(buffer);
 }
+
 int stoi(const string& str)
 {
 	std::stringstream ss{str.c_str()};
@@ -235,6 +280,7 @@ int stoi(const string& str)
 	
 	return value;
 }
+
 double stod(const string& str)
 {
 	std::stringstream ss{str.c_str()};
@@ -246,48 +292,90 @@ double stod(const string& str)
 	
 	return value;
 }
-std::size_t string::size() const
+
+string::size_type string::size() const
 {
-	if (m_onHeap) {
-		return m_onheap.size;
-	}
-	return std::strlen(m_onstack);
+	return _size;
 }
-const char* string::c_str() const
+
+string::size_type string::length() const
 {
-    return m_onHeap ? m_onheap.str : m_onstack;
+	return _size;
 }
-void string::append(const char* str) 
+
+bool string::empty() const
 {
-    std::size_t len = std::strlen(str);
-    std::size_t currentLen = size();
-   
-    if (!m_onHeap) {
-        if (currentLen + len + 1 < m_maxSize) {
-            std::strcat(m_onstack, str);
+    return _size == 0;
+}
+
+string::const_pointer string::c_str() const
+{
+    return _isOnHeap ? _onheap : _onstack;
+}
+
+string::pointer string::data() const
+{
+    return _isOnHeap ? _onheap : nullptr;
+}
+
+string::value_type string::front() const
+{
+    return _isOnHeap ? _onheap[0] : _onstack[0];
+}
+
+string::value_type string::back() const
+{
+    return _isOnHeap ? _onheap[_size - 1] : _onstack[_size - 1];
+}
+
+void string::append(const_pointer str) 
+{
+    size_type sz =  std::strlen(str) + size();
+    if (!_isOnHeap) {
+        if (sz + 1 < _maxSize) {
+            std::strcat(_onstack, str);
         }
         else {
-            m_onHeap = true;
-            std::size_t size =  len + currentLen;
-            char* tmp = new char[size + 1];
-            std::strcpy(tmp, m_onstack);
+            _isOnHeap = true;
+            char* tmp = new char[sz + 1];
+            std::strcpy(tmp, _onstack);
             std::strcat(tmp, str);
-            m_onheap.size = size;
-            m_onheap.str = tmp;
+            _onheap = tmp;
         }
     }
     else {
-        std::size_t size = len + currentLen;
-        char* tmp = new char[size + 1];
-        std::strcpy(tmp, m_onheap.str);
+        char* tmp = new char[sz + 1];
+        std::strcpy(tmp, _onheap);
         std::strcat(tmp, str);
-        m_onheap.size = size;
-        m_onheap.str = tmp;
+        _onheap = tmp;
     }
+    _size = sz;
 }
-string string::substr(std::size_t pos, std::size_t len)
+
+void string::append(const string& str)
 {
-	std::size_t length = size();
+    append(str.c_str());
+}
+
+string& string::insert(size_type pos, const_pointer str)
+{
+    if (pos > _size) {
+        throw std::out_of_range("Invalid index!");
+    }
+    if (pos == _size) {
+        append(str);
+    }
+    string tmp;
+    tmp.append(substr(0, pos));
+    tmp.append(str);
+    tmp.append(substr(pos, _size - pos));
+    *this = tmp;
+    return *this;
+}
+
+string string::substr(size_type pos, size_type len)
+{
+	size_type length = size();
 	
 	if (pos > length || len == 0) {
 		return string();
@@ -301,13 +389,65 @@ string string::substr(std::size_t pos, std::size_t len)
 	
 	return string{st};
 }
-void string::copyFrom(const string& src) {
-    m_onHeap = src.m_onHeap;
-    if (m_onHeap) {
-        m_onheap.size = src.m_onheap.size;
-        m_onheap.str = new char[m_onheap.size + 1];
-        std::strcpy(m_onheap.str, src.m_onheap.str);
-    } else {
-        std::strcpy(m_onstack, src.m_onstack);
+
+string::const_reference string::at(size_type pos) const {
+    if (pos >= _size) {
+        throw std::out_of_range("Invalid index!");
     }
+    return _isOnHeap ? _onheap[pos] : _onstack[pos];
+}
+
+string::reference string::at(size_type pos)
+{
+    return const_cast<reference>(std::as_const(*this).at(pos));
+}
+
+void string::clear() noexcept
+{
+    if (_isOnHeap) {
+        delete[] _onheap;
+    }
+    _onstack[0] = '\0';
+    _size = 0;
+    _isOnHeap = false;
+}
+
+string::iterator string::begin()
+{
+    pointer ptr = (char*)(_isOnHeap ? _onheap : _onstack);
+    return iterator{ptr};
+}
+
+string::const_iterator string::cbegin() const
+{
+    pointer ptr = (char*)(_isOnHeap ? _onheap : _onstack);
+    return const_iterator{ptr};
+}
+
+string::iterator string::end()
+{
+    pointer ptr = (char*)(_isOnHeap ? _onheap : _onstack);
+    return iterator{ptr + _size};
+}
+
+string::const_iterator string::cend() const
+{
+    pointer ptr = (char*)(_isOnHeap ? _onheap : _onstack);
+    return iterator{ptr + _size};
+}
+
+void string::copyFrom(const string& src) {
+    _isOnHeap = src._isOnHeap;
+    if (_isOnHeap) {
+        _size = src._size;
+        _onheap = new char[_size + 1];
+        std::strcpy(_onheap, src._onheap);
+    } else {
+        std::strcpy(_onstack, src._onstack);
+    }
+}
+
+string operator""_ms(string::const_pointer str, string::size_type)
+{
+    return string{str};
 }
