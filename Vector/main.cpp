0b086bf18b16eab1d813ac669cecd9c51d8e4660
Hripsime Hakobyan <118827365+Hakob1an@users.noreply.github.com>
2024-01-30 20:16:59 +0400

    Update main.cpp

--- Vector/main.cpp
+++ Vector/main.cpp
@@ -1,6 +1,14 @@
 #include "vector.hpp"
 #include <iostream>
 
+void print(vector<int>& vec) 
+{
+	for (auto& a : vec) {
+		std::cout << a << " ";
+	}
+	std::cout << std::endl;
+}
+
 int main() {
     try {
         vector<int> intVector;
@@ -13,7 +21,8 @@ int main() {
 
         std::cout << "Element at index 1: " << intVector[1] << std::endl;
 
-        std::cout << "Vector contents: " << intVector << std::endl;
+        std::cout << "Vector contents: ";
+        print(intVector);
 
         intVector.pop_back();
         std::cout << "After pop_back, size: " << intVector.size() << std::endl;
@@ -22,31 +31,38 @@ int main() {
         std::cout << "After reserving capacity 10, Capacity: " << intVector.capacity() << std::endl;
 
         intVector.insert(4, 1);
-        std::cout << "After inserting 4 at index 1: " << intVector << std::endl;
+        std::cout << "After inserting 4 at index 1: ";
+        print(intVector);
 
-        intVector.remove(1);
-        std::cout << "After removing element at index 1: " << intVector << std::endl;
+        intVector.erase(1);
+        std::cout << "After removing element at index 1: ";
+        print(intVector);
 
         vector<int> anotherVector;
         anotherVector.push_back(5);
         anotherVector.push_back(6);
         
         intVector += anotherVector;
-        std::cout << "After += operation: " << intVector << std::endl;
+        std::cout << "After += operation: ";
+        print(intVector);
 
         vector<int> copyVector(intVector);
-        std::cout << "Copy vector: " << copyVector << std::endl;
+        std::cout << "Copy vector: ";
+        print(copyVector);
 
         vector<int> moveVector(std::move(intVector));
-        std::cout << "Move vector: " << moveVector << std::endl;
+        std::cout << "Move vector: ";
+        print(moveVector);
 
         vector<int> assignedVector;
         assignedVector = copyVector;
-        std::cout << "After assignment: " << assignedVector << std::endl;
+        std::cout << "After assignment: ";
+        print(assignedVector);
 
         vector<int> movedVector;
         movedVector = std::move(copyVector);
-        std::cout << "After move assignment: " << movedVector << std::endl;
+        std::cout << "After move assignment: ";
+        print(moveVector);
         
         std::cout << std::endl;
 
@@ -71,8 +87,13 @@ int main() {
         } catch (const std::out_of_range& e) {
             std::cerr << "Caught exception: " << e.what() << std::endl;
         }
-
-        vector<int> intVector2{1, 2, 3, 4};
+		
+		//Using iterators
+        vector<int> intVector2; 
+        intVector2.push_back(1);
+        intVector2.push_back(2);
+        intVector2.push_back(3);
+        std::cout << intVector2.capacity() << std::endl;
         vector<int>::iterator it = intVector2.begin();
         for (; it != intVector2.end(); ++it) {
             std::cout << *it << " ";
@@ -84,6 +105,12 @@ int main() {
             std::cout << it[i] << " ";
         }
         std::cout << std::endl;
+        
+        //Inserting a static array
+        int arr[5] = {1, 2, 3, 4, 5};
+        vector<int> vec {1, 2, 3};
+        vec.insert(arr, 5, 2);
+        print(vec);
 
     } catch (const std::exception& e) {
         std::cerr << "Caught unexpected exception: " << e.what() << std::endl;
